a80abf38698e8b2ed95f315fcfd00630f430a283
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-10-08 04:09:36 +0530

    ADDED CLERCK

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBachground from './pages/RemoveBachground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
 
-function App() {
+const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+        <Route index element ={<Dashboard />} />
+        <Route path='write-article' element={<WriteArticle/>} />
+        <Route path='blog-titles' element={<BlogTitles/>} />
+        <Route path='generate-images' element={<GenerateImages/>} />
+        <Route path='remove-background' element={<RemoveBachground/>} />
+        <Route path='remove-objects' element={<RemoveObject/>} />
+        <Route path='review-resume' element={<ReviewResume/>} />
+        <Route path='community' element={<Community/>} />
+        
+
+
+
+
+
+
+
+
+        </Route>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/RemoveBachground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RemoveBachground = () => {
+  return (
+    <div>
+      <h1>Remove Background</h1>
+    </div>
+  )
+}
+
+export default RemoveBachground
