6e6084b4c06a14ed55b12c68858a71c05998111c
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-10-18 11:45:12 +0530

    article generator started working

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,6 +1,11 @@
 import { Edit, Sparkles } from 'lucide-react'
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast'
+
+axios.defaults.baseURL=import.meta.env.VITE_BASE_URL; 
 
 const WriteArticle = () => {
 
@@ -13,8 +18,30 @@ const articleLength= [
 
 const [selectedLength,setSelectedLength]=useState(articleLength[0])
 const [input,setInput] =useState('')
+const [loading,setLoading]=useState(false)
+const [content,setContent]=useState('')
+
+const {getToken}=useAuth();
+
 const onSubmitHandler =async (e)=> {
   e.preventDefault();
+  try{
+    setLoading(true)
+    const prompt=`Write an article about ${input} in ${selectedLength.text}`
+    const {data} =await axios.post('/api/ai/generate-article',{prompt,
+      length:selectedLength.length},{
+        headers:{Authorization:`Bearer ${await getToken()}`}
+      }
+      )
+      if(data.success){
+        setContent(data.content)
+      } else {
+        toast.error(data.message)
+      }
+  } catch (error){
+   toast.error(error.message)
+  }
+  setLoading(false)
 }
 
   return (
@@ -47,9 +74,17 @@ const onSubmitHandler =async (e)=> {
 
         </div>
         <br/>
-        <button className='w-full flex justify-center items-center gap-2
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#226BFF] to-[#65ADff] text-white px-4 py-2 mt-6
         text-sm rounded-lg cursor-pointer '>
+
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2 
+            border-t-transparent animate-spin'>
+
+            </span>
+            : <Edit className='w-5' />
+          }
           <Edit className='w-5' />
            Generate article
         </button>
@@ -66,7 +101,10 @@ const onSubmitHandler =async (e)=> {
 
         </div>
 
-        <div className='flex-1 flex justify-center items-center'>
+        {!content ? (
+         
+
+         <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5
           text-gray-400'>
             <Edit className='w-9 h-9 ' />
@@ -74,6 +112,17 @@ const onSubmitHandler =async (e)=> {
 
           </div>
         </div>
+        ):(<div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+          <div>
+           {content}
+
+          </div>
+          
+           </div>
+          
+          )}
+
+        
 
       </div>
     
--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,2 +1,5 @@
 import multer from 'multer'
 
+const storage =multer.diskStorage({});
+
+export const upload =multer({storage})
